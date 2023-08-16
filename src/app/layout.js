1ebf91a9ed07c5f5a95361e70ce45a4c2ad7c12c
zhoujianchao <zhoujianchao123456@yeah.net>
2023-08-16 00:03:27 -0400

    11

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,14 +16,4 @@ export default function RootLayout({ children }) {
     </html>
   )
 }
-export default function Page() {
-  return (
-      <Content>
-          <Bar/>
-          <div className="h-1/3 flex items-center justify-center mt-28">
-              {/* eslint-disable-next-line @next/next/no-img-element */}
-              <img src="/meanimal.jpg" alt="Me" className="lg:w-1/5 w-1/3 h-auto rounded-full"/>
-          </div>
-          </Content>
-    )
-}
+
