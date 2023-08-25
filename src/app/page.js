6cf5c3e69a81d6ce33caa09417612167570fa1c7
zhoujianchao <zhoujianchao123456@yeah.net>
2023-08-25 15:21:35 -0400

    changes

--- src/app/page.js
+++ src/app/page.js
@@ -6,13 +6,12 @@ export default function Home() {
         <Main>
             <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
             <h1 className="z-10 text-4xl 2xl:text-8xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-mono sm:text-6xl md:text-7xl whitespace-nowrap bg-clip-text">
-                Cyrus Zhou
+                Cyrus
             </h1>
             <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
 
             <span className="lg:mx-0 2xl:w-1/2 xl:w-2/3 lg:w-2/3 md:w-2/3 mx-2 w-auto my-8 animate-fade-in text-center text-gray-600 font-mono">
-                Hi, my name is Cyrus Zhou, and this is my welcome page. On this page you can find my introduction,
-                my blog, and my projects from GitHub. Hope you enjoy my website!
+            Hi, my name is Cyrus, and this is my welcome page. On this page you can find my media platform, my insights and past experiences. Hope you enjoy my website!"
             </span>
 
             <div className="grid 2xl:w-1/2 xl:w-2/3 lg:w-2/3 md:w-2/3 md:grid-cols-3 mb-32 text-center lg:mb-0 lg:grid-cols-3 lg:text-left animate-fade-in">
