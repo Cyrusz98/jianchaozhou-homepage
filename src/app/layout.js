8f207e884966223331171f7cfe16a133c9ee7d42
zhoujianchao <zhoujianchao123456@yeah.net>
2023-08-25 15:19:26 -0400

    small changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,8 +3,8 @@ import { Inter } from 'next/font/google'
 
 export const metadata = {
 
-  title: 'Hello from Cyrus Zhou',
-  description: "Hi, my name is Cyrus Zhou, and this is my welcome page. On this page you can find my introduction, my personal insights and experiences, and what I am doing thesedays. Hope you enjoy my website!",
+  title: 'Hello from Cyrus',
+  description: "Hi, my name is Cyrus, and this is my welcome page. On this page you can find my media platform, my insights and past experiences. Hope you enjoy my website!",
 }
 
 export default function RootLayout({ children }) {
