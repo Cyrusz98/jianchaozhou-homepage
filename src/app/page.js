99c2c9cd7b18eeef286ea86405fa14575e962687
zhoujianchao <zhoujianchao123456@yeah.net>
2023-08-25 15:23:42 -0400

    111

--- src/app/page.js
+++ src/app/page.js
@@ -11,7 +11,7 @@ export default function Home() {
             <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
 
             <span className="lg:mx-0 2xl:w-1/2 xl:w-2/3 lg:w-2/3 md:w-2/3 mx-2 w-auto my-8 animate-fade-in text-center text-gray-600 font-mono">
-            Hi, my name is Cyrus, and this is my welcome page. On this page you can find my media platform, my insights and past experiences. Hope you enjoy my website!"
+            Hi, my name is Cyrus, and this is my welcome page. On this page you can find my introduction, some works and past experiences. Hope you enjoy my website!"
             </span>
 
             <div className="grid 2xl:w-1/2 xl:w-2/3 lg:w-2/3 md:w-2/3 md:grid-cols-3 mb-32 text-center lg:mb-0 lg:grid-cols-3 lg:text-left animate-fade-in">
