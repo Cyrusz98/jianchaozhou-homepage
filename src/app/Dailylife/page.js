56ab470642bbabf0925a777eaae7ff2247c4607d
zhoujianchao <zhoujianchao123456@yeah.net>
2023-08-25 17:20:42 -0400

    12

--- src/app/Dailylife/page.js
+++ src/app/Dailylife/page.js
@@ -7,7 +7,7 @@ export default function Page() {
             <Bar/>
             <div className="h-1/3 flex items-center justify-center mt-28">
                 {/* eslint-disable-next-line @next/next/no-img-element */}
-                <img src="/boat.jpeg" alt="boat" className="lg:w-1/3 w-1/3 h-auto rounded-full"/>
+                <img src="/boat.jpg" alt="boat" className="lg:w-1/3 w-1/3 h-auto rounded-full"/>
             </div>
         </Content>
     )
