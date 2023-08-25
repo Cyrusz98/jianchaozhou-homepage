388e72fa384c973b3fba0e2e888516b7368e1f3b
zhoujianchao <zhoujianchao123456@yeah.net>
2023-08-25 16:13:04 -0400

    4444

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -10,18 +10,17 @@ export default function Page() {
                 <img src="/ME.jpeg" alt="Me" className="lg:w-1/5 w-1/3 h-auto rounded-full"/>
             </div>
             <div className="2xl:w-1/2 xl:w-2/3 lg:w-2/3 md:w-2/3 sm:w-full md:align-middle px-10 h-full">
-                <h1 className="text-white font-mono font-bold text-xl my-10">Introduction</h1>
-                <p className="text-white font-mono">
-                Hey, I am Jianchao Zhou, My Preferred English name is Cyrus!
+                <h1 className="text-white font-Garamond font-bold text-xl my-10">Introduction</h1>
+                <p className="text-white font-Garamond">
+                My real name is jianchao, and I got "cyrus" as my English name in 2020 during my trip to Tibet. My MBTI is ENFP.
                 </p>
                 <br/>
-                <p className="text-white font-mono">
-                I Presently enrolled as a data analytics major at Brown University. In the past 5 years, I earned a bachelor's degree in journalism from Jinan University and a master's degree in business analytics from the University of Connecticut. My academic journey reflects a diverse range of interests, particularly in public discourse such as environmental resources, technology, and urban planning. Eager to delve deeper, I aspire to conduct scholarly research within these domains. 
-                Complementing my academic pursuits, I boast a professional background within several technology enterprises, where I actively engaged in roles encompassing data analysis and product evaluation. This experience has not only fortified my analytical prowess but also honed my practical understanding of translating insights into strategic actions. In essence, I embody a unique fusion of academic enthusiasm and real-world proficiency, positioning me as a multifaceted asset poised to contribute meaningfully across both research and practical spheres.
-                </p>
+                <p className="text-white font-Garamond">
+                I am a master student at Brown University majoring in social data analytics and expect to graduate in 2024. Before that, I gained a bachelor's degree in journalism and a master's degree in business analytics.
+                I greatly enjoy engaging in discussions on the topic such as technology updated, environmental resources, and urban planning with others. 
                 <br/>
-                <p className="text-white font-mono">
-                Cyrus embodies a range of exceptional qualities. His linguistic proficiency spans Mandarin,cantonese and English, with impeccable skills in both spoken communication and written expression. Cyrus's professional demeanor extends to his remarkable self-motivation and exemplary communication prowess, enabling seamless cross-functional collaboration that ultimately drives success. His wealth of creative and innovative thinking is complemented by his robust experience in user and product research and analysis, empowering him to effectively shape and execute pivotal company decisions. He remains attuned to intricate details while effortlessly managing time, underpinned by unwavering composure under pressure and a willingness to embrace challenging assignments. Armed with strong analytical and logical thinking abilities, he is well-versed in industry regulations and market conventions, ensuring an informed and strategic approach in all endeavors.
+                <p className="text-white font-Garamond">
+                I am fluent in Mandarin Chinese and English, and I am currently studying Spanish and Japanese. I possess exceptional interdisciplinary learning abilities. I am skilled in using SQL, Python, R, and Stata for data analysis and modeling, and I also have a solid understanding of financial analysis.
                 </p>
             </div>
             <div className="h-28"></div>
