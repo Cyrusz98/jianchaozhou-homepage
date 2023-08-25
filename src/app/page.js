54f92f78e5e511f12183eb36f38d6b8eddb2560e
zhoujianchao <zhoujianchao123456@yeah.net>
2023-08-25 19:28:23 -0400

    485829

--- src/app/page.js
+++ src/app/page.js
@@ -8,20 +8,22 @@ export default function Home() {
             <h1 className="z-10 text-4xl 2xl:text-8xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-Helvetica sm:text-6xl md:text-7xl whitespace-nowrap bg-clip-text">
                 Cyrus 🐶
             </h1>
-            <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
-            <div className="grid 2xl:w-1/2 xl:w-2/3 lg:w-2/3 md:w-2/3 md:grid-cols-3 mb-32 text-center lg:mb-0 lg:grid-cols-3 lg:text-left animate-fade-in">
+            <div className="grid 2xl:w-1/2 xl:w-2/3 lg:w-2/3 md:w-2/3 md:grid-cols-3 mb-16 text-center lg:mb-0 lg:grid-cols-3 lg:text-left animate-fade-in">
                 <Card name="Introduction" href="/about"/>
-                <Card name="linkedin" href="https://www.linkedin.com/in/cyrus-zhou-0958a0157/"/>
                 <Card name="Projects" href="/projects"/>
                 <Card name="Daliylife" href="/Daliylife"/>
             </div>
-            <div className="text-center mt-16">
-                <p style={{ fontWeight: 'bold' }}>Address:</p>
-                10 park row W, Providence, RI 02903
-                <p style={{ fontWeight: 'bold' }}>Email:</p>
+            <div className="text-center">
+                <p className="font-bold">Address:</p>
+                10 Park Row W, Providence, RI 02903
+                <p className="font-bold mt-2">Email:</p>
                 jianchao_zhou@brown.edu
-                <p style={{ fontWeight: 'bold' }}>Tel:</p>
+                <p className="font-bold mt-2">Tel:</p>
                 860-8690434
+            </div>
+            <div className="mb-8"></div>
+            <div className="flex justify-center mt-8">
+                <Card name="linkedin" href="https://www.linkedin.com/in/cyrus-zhou-0958a0157/"/>
             </div>
         </Main>
     );
