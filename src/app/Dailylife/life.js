148248fb10f240c40fba40766155ce9b46afe8e0
zhoujianchao <zhoujianchao123456@yeah.net>
2023-08-25 16:50:01 -0400

    12

--- src/app/Dailylife/life.js
+++ src/app/Dailylife/life.js
@@ -7,23 +7,23 @@ export default function Page() {
             <Bar/>
             <div className="h-1/3 flex items-center justify-center mt-28">
                 {/* eslint-disable-next-line @next/next/no-img-element */}
-                <img src="/boat.jpeg" alt="Me" className="lg:w-1/3 w-1/3 h-auto rounded-full"/>
+                <img src="/boat.jpeg" alt="boat" className="lg:w-1/3 w-1/3 h-auto rounded-full"/>
             </div>
             <div className="h-1/3 flex items-center justify-center mt-28">
                 {/* eslint-disable-next-line @next/next/no-img-element */}
-                <img src="/chile.jpeg" alt="Me" className="lg:w-1/3 w-1/3 h-auto rounded-full"/>
+                <img src="/chile.jpeg" alt="chile" className="lg:w-1/3 w-1/3 h-auto rounded-full"/>
             </div>
             <div className="h-1/3 flex items-center justify-center mt-28">
                 {/* eslint-disable-next-line @next/next/no-img-element */}
-                <img src="/breath.jpeg" alt="Me" className="lg:w-1/3 w-1/3 h-auto rounded-full"/>
+                <img src="/breath.jpeg" alt="breath" className="lg:w-1/3 w-1/3 h-auto rounded-full"/>
             </div>
             <div className="h-1/3 flex items-center justify-center mt-28">
                 {/* eslint-disable-next-line @next/next/no-img-element */}
-                <img src="/agintina.jpeg" alt="Me" className="lg:w-1/3 w-1/3 h-auto rounded-full"/>
+                <img src="/agintina.jpeg" alt="agintina" className="lg:w-1/3 w-1/3 h-auto rounded-full"/>
             </div>
             <div className="h-1/3 flex items-center justify-center mt-28">
                 {/* eslint-disable-next-line @next/next/no-img-element */}
-                <img src="/meanimal.jpeg" alt="Me" className="lg:w-1/3 w-1/3 h-auto rounded-full"/>
+                <img src="/meanimal.jpeg" alt="meanimal" className="lg:w-1/3 w-1/3 h-auto rounded-full"/>
             </div>
         </Content>
     )
