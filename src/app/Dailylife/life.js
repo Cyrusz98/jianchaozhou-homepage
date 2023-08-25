75b80dcc2dce44a22e6cb590540b8d05d36d2468
zhoujianchao <zhoujianchao123456@yeah.net>
2023-08-25 16:53:03 -0400

    2

--- src/app/Dailylife/life.js
+++ src/app/Dailylife/life.js
@@ -9,22 +9,6 @@ export default function Page() {
                 {/* eslint-disable-next-line @next/next/no-img-element */}
                 <img src="/boat.jpeg" alt="boat" className="lg:w-1/3 w-1/3 h-auto rounded-full"/>
             </div>
-            <div className="h-1/3 flex items-center justify-center mt-28">
-                {/* eslint-disable-next-line @next/next/no-img-element */}
-                <img src="/chile.jpeg" alt="chile" className="lg:w-1/3 w-1/3 h-auto rounded-full"/>
-            </div>
-            <div className="h-1/3 flex items-center justify-center mt-28">
-                {/* eslint-disable-next-line @next/next/no-img-element */}
-                <img src="/breath.jpeg" alt="breath" className="lg:w-1/3 w-1/3 h-auto rounded-full"/>
-            </div>
-            <div className="h-1/3 flex items-center justify-center mt-28">
-                {/* eslint-disable-next-line @next/next/no-img-element */}
-                <img src="/agintina.jpeg" alt="agintina" className="lg:w-1/3 w-1/3 h-auto rounded-full"/>
-            </div>
-            <div className="h-1/3 flex items-center justify-center mt-28">
-                {/* eslint-disable-next-line @next/next/no-img-element */}
-                <img src="/meanimal.jpeg" alt="meanimal" className="lg:w-1/3 w-1/3 h-auto rounded-full"/>
-            </div>
         </Content>
     )
 }
