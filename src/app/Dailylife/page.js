7926d7c21da63e2c70e55037dc592aa7d82d6039
zhoujianchao <zhoujianchao123456@yeah.net>
2023-08-25 21:42:13 -0400

    bh

--- src/app/Dailylife/page.js
+++ src/app/Dailylife/page.js
@@ -15,7 +15,7 @@ export default function Dailylife() {
                     <div className="p-4 bg-white rounded-lg shadow-md">
                         <h3 className="text-xl font-semibold mb-2">Morning Routine</h3>
                         <p>
-                            My mornings usually start with a cup of tea and some light reading or music. I then head to the gym for a workout to kickstart the day.
+                            My mornings usually start with a cup of tea and some light reading. I then head to the gym for a workout to kickstart the day.
                         </p>
                     </div>
                     <div className="p-4 bg-white rounded-lg shadow-md">
