38b1ee3a0befcfd784c174c0ab12a55aed12940f
zhoujianchao <zhoujianchao123456@yeah.net>
2023-08-25 21:48:40 -0400

    的

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,16 @@
-import './globals.css';
-import { Inter } from '@next/google-fonts';
+import './globals.css'
+import { Inter } from 'next/font/google'
 
 export const metadata = {
+
   title: 'Hello from Cyrus',
   description: "Hi, my name is Cyrus, and this is my welcome page. On this page you can find my media platform, my insights and past experiences. Hope you enjoy my website!",
 }
 
 export default function RootLayout({ children }) {
   return (
-    <div>
-      <Inter /> {/* 应用 Inter 字体 */}
-      {children}
-    </div>
-  );
+    <html lang="en">
+      <body className="h-full bg-black">{children}</body>
+    </html>
+  )
 }
