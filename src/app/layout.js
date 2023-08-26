c38d8237add679afde54a094204cbc858cd2998b
zhoujianchao <zhoujianchao123456@yeah.net>
2023-08-25 21:44:42 -0400

    fjsnw

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,7 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="h-full bg-black">{children}</body>
+      <body className="h-full bg-green">{children}</body>
     </html>
   )
 }
