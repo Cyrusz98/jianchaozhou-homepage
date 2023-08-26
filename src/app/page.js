6d3c9ed32aecc13432d6311e3d1b3793a02dcb4c
zhoujianchao <zhoujianchao123456@yeah.net>
2023-08-25 20:07:59 -0400

    最

--- src/app/page.js
+++ src/app/page.js
@@ -9,12 +9,12 @@ export default function Home() {
             <h1 className="z-10 text-4xl 2xl:text-8xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-Helvetica sm:text-6xl md:text-7xl whitespace-nowrap bg-clip-text">
                 Cyrus 🐶
             </h1>
-            <br /><br /><br /><br />
+            <br /><br />
 
             <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
             <span className="lg:mx-0 2xl:w-1/2 xl:w-2/3 lg:w-2/3 md:w-2/3 mx-2 w-auto my-8 animate-fade-in text-center text-gray-600 font-Helvetica">
                 Hi, my name is Cyrus, and this is my welcome page. On this page you can find my introduction, some works and past experiences. Hope you enjoy my website!
-                <br /><br /><br />
+                <br /><br />
             </span>
             
 
@@ -24,7 +24,7 @@ export default function Home() {
                 <Card name="Projects" href="/projects"/>
                 <Card name="Daliylife" href="/Daliylife"/>
             </div>
-
+            <br /><br /><br /><br /><br />
             <p><strong>Address:</strong>10 park row W, Providence,RI 02903</p>
             <p><strong>Email:</strong> jianchao_zhou@brown.edu</p> 
             <p><strong>Tel:</strong>860-8690434</p> 
